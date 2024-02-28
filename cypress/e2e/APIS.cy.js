536a282b22c8fe16bf9d9ae8295e96b9fdce8d25
Diego Romero Mora <diego.romero@solera.com>
2024-02-28 14:40:35 -0600

    change

--- cypress/e2e/APIS.cy.js
+++ cypress/e2e/APIS.cy.js
@@ -1,4 +1,4 @@
-describe('Rick And Morty API',() =>{
+describe('API testing',() =>{
     it('Endpoint Post:200 STATUS',()=>{
         cy.visit('https://jsonplaceholder.typicode.com/').request('/posts')
     })
@@ -25,9 +25,18 @@ describe('Rick And Morty API',() =>{
             cy.log(`Número total de personajes: ${response.body.results.length}`);
           });
       });
-
+      it('Create posts wiht POST method', () => {
+        const datos = {
+          title: 'QA Automation from s',
+          body: 'This is a simple post for QA automation course',
+          userId: 1
+        };
     
+        cy.request('POST', 'https://jsonplaceholder.typicode.com/posts', datos)
+          .then((response) => {
+            expect(response.status).to.eq(201);
+          });
+      });
 
-    
 
 })
